540879b21d62a955f7d4e38d3689a02161568d88
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2020-10-27 00:53:49 +1300

    Update app.js
    Removed the private information for the contact form

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBChtqNiRhf5CZPZUFatgAHbxHZaiiW0IQ",
-    authDomain: "contact-form-a4306.firebaseapp.com",
-    databaseURL: "https://contact-form-a4306.firebaseio.com",
-    projectId: "contact-form-a4306",
-    storageBucket: "contact-form-a4306.appspot.com",
-    messagingSenderId: "122988224290",
-    appId: "1:122988224290:web:b1117051ec3d50847d8f7b"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Reference contactInfo Collections
-let contactInfo = firebase.database().ref("infos");
-
-//  end of firebase connection
 
 // show menu in mobile
 const showMenu = (toggleId, navId) => {
@@ -76,44 +59,3 @@ sr.reveal('.work_img',{interval: 200});
 sr.reveal('.contact_input',{interval: 200}); 
 
 // contact form
-// listen for a submit
-document.querySelector(".contact_form").addEventListener("submit", submitForm);
-
-function submitForm(e) {
-    e.preventDefault();
-
-    // get input values
-    let name = document.querySelector(".name").value;
-    let email = document.querySelector(".email").value;
-    let message = document.querySelector(".message").value;
-
-    saveContactInfo(name, email, message);
-
-    document.querySelector(".contact_form").reset();
-
-    sendEmail(name, email, message);
-}
-
-// save infos to Firebase
-function saveContactInfo(name, email, message) {
-    let newContactInfo = contactInfo.push();
-
-    newContactInfo.set({
-        contactName: name, 
-        contactEmail: email, 
-        contactMessage: message,
-    });
-}
-
-// send email 
-function sendEmail(name, email, message){
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: 'johnageronimo1@gmail.com',
-        Password: "vplrgujrsasbsnwt",
-        To: 'johnageronimo1@gmail.com', 
-        From: 'johnageronimo1@gmail.com',
-        Subject: `${name} sent you a message`, 
-        Body: `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`,
-    }).then((message) => alert("Mail sent successfully"));
-}
